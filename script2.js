779043c5bcafd539b790adbba8be0f5e0cbfbaed
Carlos Daniel Dias Freitas <129292741+LisoDaniel@users.noreply.github.com>
2023-09-06 17:46:05 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -8,12 +8,8 @@ var splashScreen = document.querySelector('.splash');
 function logar(){
 
     var password = document.getElementById('password').value;
-    var encrypted = CryptoJS.AES.encrypt(password, 'chave');
-    var decrypted = CryptoJS.AES.decrypt(
-      encrypted,
-      'chave'
-    );
-    if(decrypted == "50415353574f5244313131"){
+    var encrypted = CryptoJS.MD5(password);
+    if(encrypted == 'f7f1c73df4561a07bfde1ff55f448056'){
         alert('SUCESSO!!!!');
         location.href = "multiverse.html";
        }else{
