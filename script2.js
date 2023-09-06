07ad25421c28852ae0624d886da1d18fdc4a3af8
Carlos Daniel Dias Freitas <129292741+LisoDaniel@users.noreply.github.com>
2023-09-06 17:48:04 -0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -4,17 +4,3 @@ var splashScreen = document.querySelector('.splash');
     splashScreen.classList.add('hidden')
     splashScreen.style.opacity = 0;
   },11000);
-
-function logar(){
-
-    var password = document.getElementById('password').value;
-    var encrypted = CryptoJS.MD5(password);
-    if(encrypted == 'f7f1c73df4561a07bfde1ff55f448056'){
-        alert('SUCESSO!!!!');
-        location.href = "multiverse.html";
-       }else{
-        alert('Senha incorreta, tente novamente!');
-    }
-    }
-  
-   
